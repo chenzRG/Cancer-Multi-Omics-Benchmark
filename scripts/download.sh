9bf4cabc8ad1a859ae6dbae802ccdec4f100ba72
chenzRG <125750017+chenzRG@users.noreply.github.com>
2025-03-11 13:58:04 +0900

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
-#DROPBOX_LINK="https://www.dropbox.com/scl/fi/dxqvrlkaly6ncph8zhy08/Main_Dataset.zip?rlkey=lxrh4ubay9kxseholxng59kvo&st=q91p0atg&dl=0"
+# Google Drive file ID
 FILE_ID="1JF4aHuRS7_EXgjN8VySej0wNfoXn9nnQ"
 
 # Destination directory for the download
 DEST_DIR=".."
 
-# Extract the file name from the Dropbox link
-#FILE_NAME=$(basename "$DROPBOX_LINK" | cut -d'?' -f1)
+# file name
 FILE_NAME="Main_Dataset.zip"
 
-# Modify the download link (change dl=0 to dl=1)
-#DOWNLOAD_LINK="${DROPBOX_LINK/dl=0/dl=1}"
+# check gdown installation
+if ! command -v gdown &> /dev/null
+then
+    echo "gdown not found, installing..."
+    pip install gdown
+fi
 
-DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=${FILE_ID}"
-curl -c /tmp/gdrive-cookie -s -L "${DOWNLOAD_URL}" > /dev/null
-CONFIRM_CODE=$(awk '/download/ {print $NF}' /tmp/gdrive-cookie)
+# download
+gdown "https://drive.google.com/uc?export=download&id=${FILE_ID}" -O "${DEST_DIR}/${FILE_NAME}"
 
-# Download the file to the specified directory
-#curl -L -o "${DEST_DIR}/${FILE_NAME}" "$DOWNLOAD_LINK"
-curl -Lb /tmp/gdrive-cookie "https://drive.google.com/uc?export=download&confirm=${CONFIRM_CODE}&id=${FILE_ID}" -o "${DEST_DIR}/${FILE_NAME}"
+# check if download was successful
+if [[ ! -f "${DEST_DIR}/${FILE_NAME}" ]]; then
+    echo "Download failed. Please check your Google Drive link."
+    exit 1
+fi
+
+# make sure the downloaded file is a ZIP archive
+if ! file "${DEST_DIR}/${FILE_NAME}" | grep -q "Zip archive data"; then
+    echo "Downloaded file is not a ZIP archive. Something went wrong."
+    exit 1
+fi
 
 echo "File has been downloaded to ${DEST_DIR}/${FILE_NAME}."
 
-# Unzip the downloaded folder
-unzip "${DEST_DIR}/${FILE_NAME}" -d "$DEST_DIR"
+# unzip
+unzip -o "${DEST_DIR}/${FILE_NAME}" -d "$DEST_DIR"
 
-# Remove the ZIP file after extraction
+# remove the ZIP archive
 rm "${DEST_DIR}/${FILE_NAME}"
 
 echo "Main_Dataset Folder has been downloaded and extracted to ${DEST_DIR}."
+
+
